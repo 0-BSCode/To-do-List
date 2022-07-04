d80376755da180866f7320256152381dd2b37527
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-07-04 17:55:15 +0800

    Initialized services

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAwr-0dnuDcYM4VWlexNX8OWw3G93l8YHY",
   authDomain: "react-firebase-9511f.firebaseapp.com",
@@ -6,3 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "233295397608",
   appId: "1:233295397608:web:12c9790f5d36a79cdebf5d",
 };
+
+const app = initializeApp(firebaseConfig);
+
+// Services
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
