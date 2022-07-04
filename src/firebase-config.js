c93e4d2f3d12a90a0642826365694785efcc7954
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-07-04 17:51:13 +0800

    Added firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAwr-0dnuDcYM4VWlexNX8OWw3G93l8YHY",
+  authDomain: "react-firebase-9511f.firebaseapp.com",
+  projectId: "react-firebase-9511f",
+  storageBucket: "react-firebase-9511f.appspot.com",
+  messagingSenderId: "233295397608",
+  appId: "1:233295397608:web:12c9790f5d36a79cdebf5d",
+};
