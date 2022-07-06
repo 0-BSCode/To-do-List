37c151fa4347598506c4b1f3eeadfde49d4fd6b1
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-07-06 15:15:00 +0800

    Enabled editing and deleting of notes

--- src/components/Entry/index.jsx
+++ src/components/Entry/index.jsx
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Button } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { db, auth } from "../../firebase-config";
 import { signOut } from "firebase/auth";
 import {
@@ -13,22 +23,26 @@ import {
   query,
   orderBy,
 } from "firebase/firestore";
+import Note from "../Note";
 
 const Entry = () => {
   const [text, setText] = useState("");
   const [notes, setNotes] = useState([]);
+  const [disable, setDisable] = useState(false);
   const notesCollectionRef = collection(db, "notes");
 
-  console.log(auth.currentUser.uid);
   const notesQuery = query(
     notesCollectionRef,
     where("createdBy", "==", auth.currentUser.uid)
+    // orderBy("createdAt")
   );
 
   useEffect(() => {
     const unsubNotes = onSnapshot(notesQuery, (snapshot) => {
+      console.log("SNAPSHOT");
       const notes = [];
       snapshot.docs.forEach((doc) => {
+        console.log("DOC: ", { ...doc.data(), id: doc.id });
         notes.push({ ...doc.data(), id: doc.id });
       });
 
@@ -36,22 +50,29 @@ const Entry = () => {
     });
   }, []);
 
+  useEffect(() => {
+    console.log("Current User: ", auth.currentUser.uid);
+    console.log("Notes: ", notes);
+  });
+
   const saveText = async (e) => {
     e.preventDefault();
 
+    await setDisable(true);
     if (text !== "") {
       await addDoc(notesCollectionRef, {
         text,
+        sortText: text.toLowerCase(),
         createdAt: serverTimestamp(),
         createdBy: auth.currentUser.uid,
       });
       setText("");
     }
+    await setDisable(false);
   };
 
   const logout = async (e) => {
     await signOut(auth);
-    console.log(auth.currentUser);
   };
 
   return (
@@ -63,15 +84,21 @@ const Entry = () => {
         }}
       />
 
-      <Button variant={"contained"} onClick={(e) => saveText(e)}>
+      <Button
+        disabled={disable}
+        variant={"contained"}
+        onClick={(e) => saveText(e)}
+      >
         Submit
       </Button>
 
-      {/* <Grid container> */}
-      {notes.map((note) => {
-        return <div item>{note.text}</div>;
-      })}
-      {/* </Grid> */}
+      {notes.length && (
+        <>
+          {notes.map((note) => {
+            return <Note note={note} />;
+          })}
+        </>
+      )}
       <Button variant={"outlined"} onClick={(e) => logout(e)}>
         Logout
       </Button>
---
+++ src/components/Note/index.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+  TextField,
+  Button,
+} from "@mui/material";
+import { doc, updateDoc, deleteDoc } from "firebase/firestore";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { db } from "../../firebase-config";
+
+const Note = ({ note }) => {
+  const [edit, setEdit] = useState(false);
+  const [text, setText] = useState(note.text);
+  const docRef = doc(db, "notes", note.id);
+
+  const updateNote = async (e) => {
+    await updateDoc(docRef, {
+      text,
+      sortText: text.toLowerCase(),
+    });
+    setEdit(false);
+  };
+
+  const deleteNote = async (e) => {
+    await deleteDoc(docRef);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 800, mx: "auto", mb: 2 }} variant={"outlined"}>
+      {!edit ? (
+        <>
+          <CardContent>
+            <Typography variant={"h4"}>
+              {note.text} | {note.sortText}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton aria-label="Edit" onClick={(e) => setEdit(!edit)}>
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="Delete" onClick={(e) => deleteNote(e)}>
+              <DeleteIcon />
+            </IconButton>
+          </CardActions>
+        </>
+      ) : (
+        <>
+          <TextField value={text} onChange={(e) => setText(e.target.value)} />
+          <Button variant={"contained"} onClick={(e) => updateNote(e)}>
+            SUBMIT
+          </Button>
+        </>
+      )}
+    </Card>
+  );
+};
+
+export default Note;
